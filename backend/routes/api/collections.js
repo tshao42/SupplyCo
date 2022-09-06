0d505dfc2064d9cebdfe33adb07039d765d1fbe3
tshao42 <tshao0602@gmail.com>
2022-09-06 02:42:07 -0700

    delete route added

--- backend/routes/api/collections.js
+++ backend/routes/api/collections.js
@@ -13,15 +13,15 @@ const e = require('express');
 router.get('/:collectionId', asyncHandler(async function(req,res){
     // console.trace(`hit collection individual router`)
     const collectionId = req.params.collectionId;
-    const emptyCheck = await db.Collectionitem.findAll({
-        where: {
-            collectionId: collectionId
-        }
-    });
-    if (emptyCheck.length == 0){
-        await db.Collection.destroy({where: {id: collectionId}});
-        return res.json('empty collection...')
-    }
+    // const emptyCheck = await db.Collectionitem.findAll({
+    //     where: {
+    //         collectionId: collectionId
+    //     }
+    // });
+    // if (emptyCheck.length == 0){
+    //     await db.Collection.destroy({where: {id: collectionId}});
+    //     return res.json('empty collection...')
+    // }
 
     const collection = await db.Collection.findAll({
         where: {
@@ -48,4 +48,34 @@ router.get('/users/:userId', asyncHandler(async function(req,res){
     });
     return res.json(collection);
 }))
+
+//CREATE
+router.post('/', asyncHandler(async function(req,res) {
+    const temp = await req.body;
+    const tempDetail = {
+        userId: temp.userId,
+        collectionName: temp.collectionName
+    }
+
+    const detailObj = await db.Collection.build(tempDetail);
+    const newObj = await detailObj.save();
+
+    return res.json(newObj);
+}))
+
+
+//DELETE
+router.delete('/:collectionId', asyncHandler(async function (req, res) {
+    const collectionId = req.params.collectionId;
+    await db.Collectionitem.destroy({where:{collectionId: collectionId}});
+    await db.Collection.destroy({ where: {id: collectionId }});
+    return res.json(collectionId);
+}))
+//EDIT
+//this includes the editing of basic information
+
+
+//need separate routes for deleting and adding items to collections
+
+
 module.exports = router;
