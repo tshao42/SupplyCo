8a2f55eb237ccf4ca8aba6153e0f6122b1b010db
tshao42 <tshao0602@gmail.com>
2022-09-14 22:41:06 -0700

    dropdown options

--- frontend/src/components/SingleProductPage/AddProductToCollection.js
+++ frontend/src/components/SingleProductPage/AddProductToCollection.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadAllCollections } from '../../store/collection';
+import { useParams } from 'react-router-dom';
+import { addItemToCollection, loadAllCollections } from '../../store/collection';
 
 
-function AddProductToCollection({setShowAddToCollectionStatu, setShowAddToCollectionButton}){
+function AddProductToCollection({setShowAddToCollectionStatus, setShowAddToCollectionButton}){
 
     const dispatch = useDispatch();
+
+    const {productId} = useParams();
 
     const [loaded, setLoaded] = useState(false);
     const collections = useSelector (state => state?.collections);
@@ -25,20 +28,49 @@ function AddProductToCollection({setShowAddToCollectionStatu, setShowAddToCollec
     const handleSubmit = async e => {
         e.preventDefault();
         //actions pertaining to submission of collection
+        let errors = [];
+        if (collectionId===0) errors.push("Please select a collection");
+        dispatch(addItemToCollection(collectionId, parseInt(productId) ));
+        setShowAddToCollectionButton(true);
+        setShowAddToCollectionStatus(false);
     }
     return (
         loaded &&
         <>
         {/* {console.log('hitting AddProductToCollection Line7')} */}
-            <form onSubmit={handleSubmit}>
-                <select name="collections">
-                    <option>Add To Existing Collection</option>
+            <form>
+                <select name="collections"
+                onChange={
+                    e=>{
+                        setCollectionId(e.target.value)
+                    }
+                }>
+                    <option value={0}>Add To Existing Collection</option>
                     {Object.values(collections).map(({id, collectionName})=>{
+                        {console.log(collections[id])}
+                        if (collections[id].Collectionitems[productId]){
                         return(
-                            <option value={id}>{collectionName}</option>
+                            <option 
+                                value={id}
+                                disabled
+                            >
+                                {collectionName}
+                            </option>
                         )
+                        } else{
+                            return(
+                                <option 
+                                    value={id}
+                                >
+                                    {collectionName}
+                                </option>
+                            ) 
+                        }
+                        
                     })}
                 </select>
+                <button onClick={handleSubmit}>Add</button>
+                <br />
                 <button>Add To New Collection</button>
                 <br />
                 <button onClick={(e)=>{
